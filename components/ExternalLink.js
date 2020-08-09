60d2d8820165ef7d8f3b5e6f99802a40d20b6b70
Arvin Wiyono <arvinwiyono@gmail.com>
2020-08-09 18:16:44 +1000

    Add visited & focus state color of links

--- components/ExternalLink.js
+++ components/ExternalLink.js
@@ -4,17 +4,10 @@ export default function ExternalLink({ url, children }) {
       <a
         href={url}
         target="_blank"
-        className="underline font-semibold transition-colors ease-in-out duration-300"
+        className="underline font-semibold transition-colors ease-in-out duration-300 focus:text-gray-600 visited:text-gray-600 hover:text-pink-300"
       >
         {children}
       </a>
-      <style jsx>
-        {`
-          a:hover {
-            color: pink;
-          }
-        `}
-      </style>
     </>
   );
 }
